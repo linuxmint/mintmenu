8da9a01db55b4f1d0c3941c199318d4d9f6cf1c4
Clement Lefebvre <clement.lefebvre@linuxmint.com>
2012-11-02 15:01:12 +0000

    Launch mintinstall as root

--- usr/lib/linuxmint/mintMenu/plugins/system_management.py
+++ usr/lib/linuxmint/mintMenu/plugins/system_management.py
@@ -131,7 +131,7 @@ class pluginclass( object ):
         if ( self.showSoftwareManager == True ):
             if os.path.exists("/usr/lib/linuxmint/mintInstall/icon.svg"):
                 Button1 = easyButton( "/usr/lib/linuxmint/mintInstall/icon.svg", self.iconsize, [_("Software Manager")], -1, -1 )
-                Button1.connect( "clicked", self.ButtonClicked, "mintinstall" )
+                Button1.connect( "clicked", self.ButtonClicked, "gksu mintinstall" )
                 Button1.show()
                 self.systemBtnHolder.pack_start( Button1, False, False )
                 self.mintMenuWin.setTooltip( Button1, _("Browse and install available software") )
