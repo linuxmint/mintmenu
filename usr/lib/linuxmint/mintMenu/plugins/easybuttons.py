469eaf57e54d6db96f4a9c098907efea136f09b2
Clement Lefebvre <clement.lefebvre@linuxmint.com>
2018-03-13 15:05:17 +0000

    mint-remove-application: Don't use gksu
    mint-common now uses pkexec internally

--- usr/lib/linuxmint/mintMenu/plugins/easybuttons.py
+++ usr/lib/linuxmint/mintMenu/plugins/easybuttons.py
@@ -380,7 +380,7 @@ class ApplicationLauncher( easyButton ):
                 Execute(self.appExec, self.appPath)
 
     def uninstall (self, *args ):
-        Execute("gksu /usr/lib/linuxmint/common/mint-remove-application.py " + self.desktopFile)
+        Execute("mint-remove-application " + self.desktopFile)
 
     # IconTheme changed, setup new icons for button and drag 'n drop
     def iconChanged( self ):
