4742162756bee5b355b002e6a729a073a801121b
gm10 <13855078+gm10@users.noreply.github.com>
2019-02-12 12:54:12 +0100

    Add execute suggestions on any length entry, but only when they do not match a shown desktop file's command

--- usr/lib/linuxmint/mintMenu/plugins/applications.py
+++ usr/lib/linuxmint/mintMenu/plugins/applications.py
@@ -203,7 +203,7 @@ class pluginclass(object):
         self.categoriesBox = self.builder.get_object("categoriesBox")
         self.favoritesBox = self.builder.get_object("favoritesBox")
         self.applicationsScrolledWindow = self.builder.get_object("applicationsScrolledWindow")
-
+        self.headingstocolor = [self.builder.get_object("label6"), self.builder.get_object("label2")]
         self.numApps = 0
 
         # These properties are NECESSARY to maintain consistency
